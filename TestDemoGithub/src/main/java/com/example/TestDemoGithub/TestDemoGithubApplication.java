fb13f3bbe597af9088cf8f625ff038ae79d64dbb
jagan353 <jaganmohanreddyk19@gmail.com>
2021-12-04 19:09:39 +0530

    initial commit

---
+++ TestDemoGithub/src/main/java/com/example/TestDemoGithub/TestDemoGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestDemoGithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDemoGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDemoGithubApplication.class, args);
+	}
+
+}
